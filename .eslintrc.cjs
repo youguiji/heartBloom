44ce1e2a4cdf2e586cca642de89153a67f672f9f
你要去的地方四季如春 <10414009+youguiji@user.noreply.gitee.com>
2023-02-28 20:00:02 +0800

    add

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,11 +4,26 @@ require("@rushstack/eslint-patch/modern-module-resolution");
 module.exports = {
   root: true,
   extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/eslint-config-prettier",
+      'plugin:vue/essential',
+      "eslint:recommended",
+      "@vue/eslint-config-prettier",
+      // '@vue/standard'
   ],
   parserOptions: {
-    ecmaVersion: "latest",
+      parser: 'babel-eslint'
   },
-};
+  rules: {
+      'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+      'space-before-function-paren': 0,
+      //'eqeqeq': false,
+      'vue/valid-template-root': false,
+      'spaced-comment': false,
+      'quotes': false,
+      'eol-last': false,
+      'key-spacing': false,
+      'vue/valid-v-for':false,
+      'vue/no-unused-vars':false,
+      'vue/no-parsing-error':false
+  }
+}
---
+++ src/views/login.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="box">
+    <div class="title">HeartBloom</div>
+    <div class="littleTile">让心开花</div>
+    <div class="loginBox">
+      <div class="tab" v-for="item in state.tab" :key="item.index">
+        <div :class="{ active: item.isActive }" class="tabItem">
+          <span>{{ item.name }}</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { reactive, ref } from "vue";
+
+export default {
+  setup() {
+    const tab = ref([
+      {
+        index: 1,
+        name: "密码登录",
+        isActive: true,
+      },
+      {
+        index: 2,
+        name: "验证码登录",
+        isActive: false,
+      },
+    ]);
+    const state = reactive({
+      tab: [
+        {
+          index: 1,
+          name: "密码登录",
+        },
+        {
+          index: 2,
+          name: "验证码登录",
+        },
+      ],
+    });
+    return {
+      state,
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+.box {
+  width: 100%;
+  height: 100%;
+  background-color: var(--bg);
+  .title {
+    font-size: 48px;
+    text-align: center;
+    font-weight: 700;
+    color: var(--themeColor);
+  }
+  .littleTile {
+    font-size: 24px;
+    text-align: center;
+    font-weight: 700;
+    color: var(--themeColor);
+  }
+  .loginBox {
+    width: 700px;
+    height: 500px;
+    background-color: #fff;
+    margin: 10px auto;
+
+    .tabBox {
+      width: 80%;
+      height: 50px;
+      .tabItem {
+        width: 30%;
+        background-color: pink;
+      }
+    }
+  }
+}
+</style>
